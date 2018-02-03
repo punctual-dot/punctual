57e1e035f0c77aef0db6eab83647dab3ce1feffc
Jang Yurai <jangyjang@gmail.com>
2018-02-03 11:06:25 -0600

    controller file

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,6 +1,6 @@
 const db = require("../models");
 
-// Defining methods for the booksController
+// Defining methods for the userController
 module.exports = {
   create: function(req, res) {
     db.User
