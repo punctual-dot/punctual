60261458eb60fb22a3247ee5530098bc225ceaf9
hnwangwa <hollynwangwa@gmail.com>
2018-02-07 20:43:42 -0600

    authentication smorgsbord
    authentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,40 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React from 'react';
+import { Route, Router } from 'react-router-dom';
 import Main from "./pages/Main";
 import Nav from "./components/Nav";
 import Profile from "./pages/Profile";
 import Food from "./pages/Food";
 import BigCalendar from "./pages/BigCalendar";
 import Toggle from "./pages/Toggle/Toggle.js";
+import Callback from "./Callback/Callback.js";
+import Auth from "./Auth/Auth";
+import history from "./history";
+import AppAuth from "./components/AppAuth/AppAuth.js";
+
+const auth = new Auth();
+
+const handleAuthentication = (nextState, replace) => {
+  if (/access_token|id_token|error/.test(nextState.location.hash)) {
+    auth.handleAuthentication();
+  }
+}
+
 
 const App = () => (
-  <Router>
+  <Router history={history}>
     <div>
       <Nav />
-      <Switch>
-        <Route exact path="/" component={Main} />
-        <Route exact path="/profile" component={Profile} />
-        <Route exact path="/food" component={Food} />
-        <Route exact path="/bigcalendar" component={BigCalendar} />
-        <Route exact path="/toggle" component={Toggle} />
-        <Route component={Main} />
-      </Switch>
+      {/*<Route exact path="/" component={Main} />*/}
+      <Route path="/" render={(props) => <AppAuth auth={auth} {...props} />} />
+      <Route path="/home" render={(props) => <Main auth={auth} {...props} />} />
+      <Route path="/callback" render={(props) => {
+        handleAuthentication(props);
+        return <Callback {...props} /> 
+      }}/>
+      <Route exact path="/profile" component={Profile} />
+      <Route exact path="/food" component={Food} />
+      <Route exact path="/bigcalendar" component={BigCalendar} />
+      <Route exact path="/toggle" component={Toggle} />
     </div>
   </Router>
 );
---
+++ client/src/Auth/auth0-variables.js
@@ -0,0 +1,5 @@
+export const AUTH_CONFIG = {
+  domain: 'punctual.auth0.com',
+  clientId: '2T0gEcUzZnl-zvU46S5Y41n_1lX-TdB5',
+  callbackUrl: 'http://localhost:3000/callback'
+}
