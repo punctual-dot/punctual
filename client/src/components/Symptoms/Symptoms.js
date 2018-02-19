48e4a47c26bfb05a6f637b7fad3beb4a1f33b8e5
Jang Yurai <jangyjang@gmail.com>
2018-02-19 10:44:48 -0600

    tiny fixes

--- client/src/components/Symptoms/Symptoms.js
+++ client/src/components/Symptoms/Symptoms.js
@@ -2,25 +2,25 @@ import React, { Component } from "react";
 import API from "../../utils/API"
 import {FormBtn} from "../../components/Form";
 
-let UserId = window.location.pathname.replace('/profile/','');
+let userId = window.location.pathname.replace('/profile/','');
 
 class Symptoms extends Component {
     constructor(props){
-    super(props)
-    this.state = {
-        symptoms: [],
-        symptom:'',
-        user:'',
-        advice: '',
-        foods: []
-    }
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleFormSubmit = this.handleFormSubmit.bind(this);
-    this.loadSymptoms = this.loadSymptoms.bind(this)
-    this.loadSymptom = this.loadSymptom.bind(this)
-    this.handleFoodClick = this.handleFoodClick.bind(this)
-    this.loadUser = this.loadUser.bind(this)
-};
+        super(props)
+        this.state = {
+            symptoms: [],
+            symptom:'',
+            user:'',
+            advice: '',
+            foods: []
+        }
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleFormSubmit = this.handleFormSubmit.bind(this);
+        this.loadSymptoms = this.loadSymptoms.bind(this)
+        this.loadSymptom = this.loadSymptom.bind(this)
+        this.handleFoodClick = this.handleFoodClick.bind(this)
+        this.loadUser = this.loadUser.bind(this)
+    };
 
     componentDidMount() {
 	    this.loadSymptoms();
@@ -33,15 +33,15 @@ class Symptoms extends Component {
         .catch(err => console.log(err))
     }
 
-    loadSymptom = (sympid) => {
-        API.getSymptom(sympid)
+    loadSymptom = (symptomId) => {
+        API.getSymptom(symptomId)
         .then(res => 
             this.setState({advice: res.data.advice, foods: res.data.foods, symptom: ""}))
         .catch(err => console.log(err))
     }
 
     loadUser = id => {
-        API.getUser(UserId)
+        API.getUser(userId)
         .then(res =>
             this.setState({user: res.data}))
         .catch(err => console.log(err))
@@ -62,7 +62,7 @@ class Symptoms extends Component {
         if (this.state.symptom) {
             for (var i=0; i < this.state.symptoms.length; i ++ ) {
                 if (this.state.symptoms[i].symptom.indexOf(this.state.symptom) >= 0) {
-                    const sympid = this.state.symptoms[i]._id
+                    const symptomId = this.state.symptoms[i]._id
                     API.addSymptomtoUser (this.state.user.id, {
                         symptoms: [
                             {   
@@ -72,9 +72,9 @@ class Symptoms extends Component {
                             }
                         ]
                     })
-                    .then(res => this.loadUser(UserId))
+                    .then(res => this.loadUser(userId))
                     .catch(err => console.log(err))
-                    this.loadSymptom(sympid)
+                    this.loadSymptom(symptomId)
                 }
             }
         }
@@ -84,7 +84,7 @@ class Symptoms extends Component {
     event.preventDefault();
         let food = event.target.dataset.food;
         this.props.setFood(food);
-     }
+    }
 
 
     render() {
@@ -110,7 +110,7 @@ class Symptoms extends Component {
                 <div id="result">
                     {this.state.user.name}
                     {this.state.advice}
-                    {this.state.foods.map(food => (]
+                    {this.state.foods.map(food => (
     				    <ul>    
                             <li className="item"> 
     				            <a onClick={this.handleFoodClick} data-food={food}>{food}</a>
