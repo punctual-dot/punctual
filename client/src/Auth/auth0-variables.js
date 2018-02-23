5f84742dc26df382124cfa32e6dd05c7bd7264eb
hnwangwa <hollynwangwa@gmail.com>
2018-02-22 21:57:46 -0600

    taking out localhost callback...
    seeing if heroku is responding to this

--- client/src/Auth/auth0-variables.js
+++ client/src/Auth/auth0-variables.js
@@ -1,5 +1,4 @@
 export const AUTH_CONFIG = {
   domain: 'punctual.auth0.com',
   clientId: '2T0gEcUzZnl-zvU46S5Y41n_1lX-TdB5',
-  callbackUrl: 'https://punctualapp.herokuapp.com/callback' || 'http://localhost:3000/callback'
-}
+  callbackUrl: 'https://punctualapp.herokuapp.com/callback'
